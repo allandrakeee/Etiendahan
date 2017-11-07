98ba99d145e333f0cabb6753ba98043bb21f5b53
allandrakeee <allandulay69@gmail.com>
2017-11-07 00:33:39 -0800

    Added to source code.

---
+++ dist/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web Development Starter Pack</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	
+	<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	 -->
+
+	<!-- For development only | Comment or delete this line of code when its production -->
+	<link rel="stylesheet" href="assets/css/theme.css">
+	
+	<!-- Uncomment this if the project is ready in production -->
+	<!-- <link rel="stylesheet" href="assets/css/theme.min.css"> -->
+</head>
+<body>
+	
+	<nav class="navbar navbar-expand-md navbar-light pt-0 pb-0 brand-primary-faded-bg">
+		<div class="container navbar-upper-inner">
+		    <div class="navbar-nav">
+				<a class="nav-item nav-link text-white" href="#"><span class="font-weight-500">Seller Centre</span></a>
+				<span class="text-white font-weight-200">|</span>
+				<a class="nav-item nav-link text-white pointer-text"><span class="font-weight-200">Follow Us On</span></a>
+				<a class="nav-item nav-link text-white font-weight-200" target="_blank" href="https://www.facebook.com"><span class="fa-animate"><i class="fa fa-facebook"></i></span></a>
+				<a class="nav-item nav-link text-white font-weight-200" target="_blank" href="https://www.instagram.com"><span class="fa-animate"><i class="fa fa-instagram"></i></span></a>
+			</div>
+
+			<div class="navbar-nav">
+				<a class="nav-item nav-link text-white" href="#"><span class="font-weight-500">Sign Up</span></a>
+				<span class="text-white font-weight-200">|</span>
+				<a class="nav-item nav-link text-white" href="#"><span class="font-weight-500">Login</span></a>
+			</div>
+	    </div>
+	</nav>
+
+	<nav class="navbar navbar-expand-md navbar-light brand-primary-bg">
+		<div class="container">
+		    <a class="navbar-brand pr-5" href="#">
+				<img src="http://via.placeholder.com/150x50" class="d-inline-block align-top">
+			</a>
+	        <form class="mx-2 my-auto d-inline w-100">
+	            <div class="input-group">
+	                <input type="text" class="form-control" placeholder="Search for products, brands, shops and locations">
+	                <span class="input-group-addon"><span class="fa fa-search"></span></span>
+	            </div>
+	        </form>
+	        <ul class="navbar-nav pl-5 pr-5">
+	            <i class="fa fa-shopping-cart fa-lg"></i>
+	        </ul>
+	    </div>
+	</nav>
+
+
+
+
+
+
+
+
+
+
+	<script src="assets/js/theme.js"></script>
+
+ 	<!-- Uncomment this if the project is ready in production -->
+	<!-- <script src="assets/js/theme.min.js"></script></body> -->
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,194 @@
+/**
+ * Tasks for gulpfile
+ * 
+ * @author Allan Drake
+ */
+
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    sassGlob    = require('gulp-sass-glob'),
+    uglify      = require('gulp-uglify'),
+    uglifycss   = require('gulp-uglifycss');
+    prefix      = require('gulp-autoprefixer'),
+    concat      = require('gulp-concat'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    livereload  = require('gulp-livereload'),
+    gutil       = require('gulp-util'),
+    del         = require('del'),
+    browserSync = require('browser-sync').create();
+
+function errorLog(error) {
+    console.error(error.message);
+}﻿
+
+// ================================ Styles Development ================================
+
+/**
+ * Development in styles
+ *
+ * Command: gulp styles
+ * @description Compile the styles in normal compile
+ */
+gulp.task('styles', function(){
+    return gulp.src([
+        'node_modules/bootstrap/scss/bootstrap.scss', 
+        'node_modules/font-awesome/scss/*.scss', 
+        'assets-dev/sass/*.scss'
+
+    ])
+    .pipe(sassGlob())
+    .pipe(sass())
+    .on('error', errorLog)
+    .pipe(concat('theme.css'))
+    .pipe(prefix('last 2 versions'))
+    .pipe(gulp.dest("assets/css"))
+    .pipe(livereload());
+});
+
+// ================================ Styles Production ================================
+
+/**
+ * Production in styles
+ *
+ * Command: gulp styles-min
+ * @description Compile the styles in uglifycss compile
+ */
+gulp.task('styles-min', function(){
+ return gulp.src([
+     'node_modules/bootstrap/scss/bootstrap.scss', 
+     'node_modules/font-awesome/scss/*.scss', 
+     'assets-dev/sass/*.scss'
+ ])
+ .pipe(sassGlob())
+ .pipe(sass())
+ .on('error', errorLog)
+ .pipe(concat('theme.min.css'))
+ .pipe(prefix('last 2 versions'))
+ .pipe(uglifycss({
+     "maxLineLen": 100,
+     "uglyComments": true
+ }))
+ .pipe(gulp.dest("assets/css"))
+});
+
+// ================================ Scripts Development ================================
+
+/**
+ * Development in scripts
+ *
+ * Command: gulp scripts
+ * @description Compile the scripts in normal compile
+ */
+gulp.task('scripts', function(){
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.js', 
+        'node_modules/popper.js/dist/umd/popper.js', 
+        'node_modules/bootstrap/dist/js/bootstrap.js', 
+        'node_modules/tether/dist/js/tether.js', 
+        'assets-dev/js/*.js'
+    ])
+    .on('error', errorLog)
+    .pipe(concat('theme.css'))
+    .pipe(gulp.dest("assets/js"))
+    .pipe(livereload());
+});
+
+// ================================ Scripts Production ================================
+
+/**
+ * Production in scripts
+ *
+ * Command: gulp scripts-min
+ * @description Compile the scripts in uglify compile
+ */
+gulp.task('scripts-min', function(){
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.js', 
+        'node_modules/popper/dist/umd/popper.js', 
+        'node_modules/bootstrap/dist/js/bootstrap.js', 
+        'node_modules/tether/dist/js/tether.js', 
+        'assets-dev/js/*.js'
+    ])
+    .on('error', errorLog)
+    .pipe(concat('theme.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest("assets/js"));
+});
+
+// ================================ Fonts Assets ================================
+
+/**
+ * Command: gulp fonts
+ * @description Clear distribution folder
+ */
+gulp.task('fonts', function () {
+    return gulp.src([
+        'node_modules/font-awesome/fonts/**/*.{ttf,woff,woff2,eof,svg}', 
+    ])
+    .pipe(gulp.dest("assets/fonts"));
+});
+
+// ================================ Watch Assets ================================
+
+/**
+ * Command: gulp watch
+ * @description Compile first all styles and scripts
+ * before starting the watcher.
+ */
+gulp.task('watch', ['styles', 'scripts'], function(){
+    livereload.listen();
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'assets-dev/sass/**/*.scss', 'assets/css/'], ['styles']);
+    gulp.watch(['node_modules/bootstrap/dist/js/bootstrap.js', 'node_modules/jquery/dist/jquery.js', 'node_modules/tether/dist/js/tether.js', 'assets-dev/js/*.js', 'assets/js'], ['scripts']);
+    gulp.watch('*.php').on('change', function(file) {
+        livereload.changed(file.path);
+    });
+});
+
+// ================================ Clear Distribution ================================
+
+/**
+ * Command: gulp clear-dist
+ * @description Clear distribution folder
+ */
+gulp.task('clear-dist', function () {
+    return del(['dist/**/*',]);
+});
+
+// ================================ Clean Development ================================
+
+/**
+ * Command: gulp dist
+ * @description Generates a clean theme for distribution
+ */
+gulp.task('dist', ['clear-dist'], function() {
+    gulp.src([
+        '**/*',
+        '!assets-dev/',
+        '!assets-dev/**',
+        '!bower_components',
+        '!bower_components/**',
+        '!node_modules',
+        '!node_modules/**',
+        '!src',
+        '!src/**',
+        '!dist',
+        '!dist/**',
+        '!dist-product',
+        '!dist-product/**',
+        '!gulp',
+        '!gulp/**',
+        '!sass',
+        '!sass/**',
+        '!readme.txt',
+        '!readme.md',
+        '!package.json',
+        '!package-lock.json',
+        '!gulpfile.js',
+        '!CHANGELOG.md',
+        '!.travis.yml',
+        '!jshintignore',
+        '!codesniffer.ruleset.xml',
+        '*'
+    ])
+    .pipe(gulp.dest('dist/'))
+});
